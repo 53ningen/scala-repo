736488674cab76ca00b112d4c23b323e94d3ee8a
53nigen <korun24276@gmail.com>
2015-01-15 23:25:24 +0900

    currying

--- src/main/scala/com/ningen/gomi/readermonad/client/HogeClientImpl.scala
+++ src/main/scala/com/ningen/gomi/readermonad/client/HogeClientImpl.scala
@@ -4,7 +4,7 @@ import java.sql.{Connection, DriverManager}
 
 class HogeClientImpl {
   
-  def setUserPassword(id: String, password: String, c: Connection): Unit = {
+  def setUserPassword(id: String, password: String): Connection => Unit = c => {
     val stmt = c.prepareStatement("update users set pwd = ? where id = ?")
     stmt.setString(1, password)
     stmt.setString(2, id)
