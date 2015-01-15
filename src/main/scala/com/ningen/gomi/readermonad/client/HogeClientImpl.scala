ab0201416a34b0d29348dc63c328ba720c468c3d
53nigen <korun24276@gmail.com>
2015-01-15 23:19:57 +0900

    refinement

--- src/main/scala/com/ningen/gomi/readermonad/client/HogeClientImpl.scala
+++ src/main/scala/com/ningen/gomi/readermonad/client/HogeClientImpl.scala
@@ -1,18 +1,22 @@
 package com.ningen.gomi.readermonad.client
 
-import java.sql.DriverManager
+import java.sql.{Connection, DriverManager}
 
 class HogeClientImpl {
   
   def setUserPassword(id: String, password: String): Unit = {
-    Class.forName("org.sqlite.JDBC")
-    val c = DriverManager.getConnection("jdbc:sqlite:memory:")
+    val c = ConnectionFactory.getConnection
     val stmt = c.prepareStatement("update users set pwd = ? where id = ?")
     stmt.setString(1, password)
     stmt.setString(2, id)
     stmt.executeUpdate()
-    c.commit()
-    c.close()
+    stmt.close()
   }
 
+}
+
+object ConnectionFactory {
+  
+  def getConnection: Connection = DriverManager.getConnection("jdbc:sqlite:memory:")
+  
 }
